27a232466d7e24f7e72c3fc13b2b8b4ea38c721e
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-10 16:29:52 -0400

    Fix eslint linter

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const hamburguer = document.querySelector('.hamburguer');
 const navMenu = document.querySelector('.nav_menu');
 hamburguer.addEventListener('click', () => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburguer.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
+
 navMenu.addEventListener('click', () => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburguer.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
