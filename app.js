c9a774615d80afab39ce83cb4c4ee0d586878860
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-04-10 15:55:33 -0400

    Add menu mobile and styles

---
+++ app.js
@@ -0,0 +1,12 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav_menu');
+
+hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+navMenu.addEventListener('click', () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
